ed6d12c0bf5ad9edd26e8088e96748de789bab96
Megan <moorem86@gmail.com>
2023-06-13 11:39:45 -0400

    final working code

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -188,14 +188,16 @@ def show_exam_result(request, course_id, submission_id):
     course = get_object_or_404(Course, pk=course_id)
     submission = Submission.objects.get(id=submission_id)
     choices = submission.choices.all()
+
+    total_possible=7
+
     total_score = 0
-    total_possible=0
-    for choice in choices:
-        total_possible += int(choice.question.grade)    
+    
+    for choice in choices:  
         if choice.is_correct:
             total_score += int(choice.question.grade)
     context['course'] = course
-    context['grade'] = total_possible #round(total_score/total_possible*100)
+    context['grade'] = round(total_score/total_possible*100)
     context['choices'] = choices
 
     return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
